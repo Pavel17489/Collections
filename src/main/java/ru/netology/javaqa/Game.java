0f837b1f1c70248ffa2f5e71a475e16b0bfd055a
Pavel <mankov.174@gmail.com>
2024-12-15 22:52:33 +0500

    Added exercise №2

--- src/main/java/ru/netology/javaqa/Game.java
+++ src/main/java/ru/netology/javaqa/Game.java
@@ -1,40 +1,41 @@
 package ru.netology.javaqa;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Game {
 
-    List<Player> players = new ArrayList<>();
+    HashMap<String, Integer> players = new HashMap<>();
 
     public void register(Player player) {
-        players.add(player);
+        players.put(player.getName(), player.getStrength());
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = null;
-        Player player2 = null;
-        for (Player player : players) {
-            if (player.getName().equals(playerName1)) {
-                player1 = player;
-            }
-            if (player.getName().equals(playerName2)) {
-                player2 = player;
-            }
-        }
+        Integer player1 = findByName(playerName1);
+        Integer player2 = findByName(playerName2);
+
         if (player1 == null){
             throw new NotRegisteredException(playerName1);
         }
         if (player2 == null){
             throw new NotRegisteredException(playerName2);
         }
-        if (player1.getStrength() > player2.getStrength()){
+        if (player1 > player2){
             return 1;
-        } else if (player1.getStrength() < player2.getStrength()){
+        } else if (player1 < player2){
             return 2;
         } else {
             return 0;
         }
+    }
+
+    public Integer findByName(String name) {
+        if (players.get(name) != null) {
+            return players.get(name);
+        }
+        return null;
     }
 
 }
